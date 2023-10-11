f4845b6926a48152426f80918eda6fc2afc7ff39
tyler-kunce <tyler.kunce@gmail.com>
2023-10-11 14:57:28 -0500

    Building Pokedex
    Building Pokedex through pokemonList array with each Pokemon and their stats as an object.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//Pokedex List
+let pokemonList = [
+    {number: '#1', name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
+    {number: '#2', name: 'Ivysaur', height: 1, type: ['Grass', 'Poison']},
+    {number: '#3', name: 'Venusaur', height: 2, type: ['Grass', 'Poison']},
+    {number: '#4', name: 'Charmander', height: 0.6, type: 'Fire'},
+    {number: '#5', name: 'Charmeleon', height: 1.1, type: 'Fire'},
+    {number: '#6', name: 'Charizard', height: 1.7, type: ['Fire', 'Flying']}
+    {number: '#7', name: 'Squirtle', height: 0.5, type: 'Water'},
+    {number: '#8', name: 'Wartortle', height: 1, type: 'Water'},
+    {number: '#9', name: 'Blastoise', height: 1.6, type: 'Water'}
+];
